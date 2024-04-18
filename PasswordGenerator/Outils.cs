e0959eb858b1d2eabb3532ccacdb964bbb9a5761
Rodelin J Cesar <cesarrodelin@outlook.com>
2024-04-17 20:16:51 -0500

    Add project files.

---
+++ PasswordGenerator/Outils.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormationCS
+{
+    static class Outils
+    {
+        public static int AskPositiveNumberNonZero(string question)
+        {
+            return AskNumberBetweenRange(question, 1, int.MaxValue, "ERROR: Value should be positive and non null");
+        }
+
+        public static int AskNumberBetweenRange(string question, int min, int max, string customMessage = null)
+        {
+            int number = AskNumber(question);
+
+            if (number >= min && number <= max)
+            {
+                return number;
+            }
+
+            if (customMessage != null)
+            {
+                Console.WriteLine(customMessage);
+            }
+            else
+            {
+                Console.WriteLine($"ERROR: Input should be between {min} and {max}!");
+            }
+
+            Console.WriteLine("------");
+            Console.WriteLine();
+
+            return AskNumberBetweenRange(question, min, max, customMessage);
+        }
+
+        public static int AskNumber(string question)
+        {
+            while (true)
+            {
+                Console.Write($"{question} --> ");
+                string? response = Console.ReadLine();
+
+                Console.WriteLine();
+                Console.WriteLine("------");
+
+                try
+                {
+                    int length = int.Parse(response);
+
+                    return length;
+                }
+                catch
+                {
+                    Console.WriteLine("ERROR: Only accepting numbers.");
+                }
+
+                Console.WriteLine("------");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ PasswordGenerator/Program.cs
@@ -0,0 +1,56 @@
+﻿using FormationCS;
+using System;
+using System.Globalization;
+using System.Runtime.Serialization;
+
+namespace PasswordGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const int AMOUNT_OF_PASSWORD = 10;
+
+            int lengthPass = Outils.AskPositiveNumberNonZero("Longueur du mot de passe");
+
+            int choice = Outils.AskNumberBetweenRange("Choose your specification: \n" +
+                "\t1- Only uppercase characters\n" +
+                "\t2- Only lowercase and uppercase characters\n" +
+                "\t3- Only characters and numbers\n" +
+                "\t4- Characters, numbers andd specials characters\n" +
+                "Votre choix", 1, 4);
+
+            string lowerCase = "abcdefghijklmnopqrstuvwxyz";
+            string upperCase = lowerCase.ToUpper();
+            string digits = "0123456789";
+            string specialCharacters = "@#$+-";
+            string alphabet;
+            string password = "";
+            Random random = new Random();
+
+            if (choice == 1)
+                alphabet = upperCase;
+            else if (choice == 2)
+                alphabet = lowerCase + upperCase;
+            else if (choice == 3)
+                alphabet = lowerCase + lowerCase + digits;
+            else
+                alphabet = upperCase + lowerCase + digits + specialCharacters;
+
+            int lengthAlphabet = alphabet.Length;
+
+            for (int i = 0; i < AMOUNT_OF_PASSWORD; i++)
+            {
+                password = "";
+
+                while (password.Length < lengthPass)
+                {
+                    int index = random.Next(0, lengthAlphabet);
+                    password += alphabet[index];
+                }
+
+                Console.WriteLine($"Password generated: {password}");
+            }
+        }
+    }
+}
